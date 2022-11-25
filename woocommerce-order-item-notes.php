8e6fbc4dcae2e2a8bdeed3388269ed02dbd0e2db
Peter Morlion <peter.morlion@gmail.com>
2022-11-25 15:33:48 +0100

    Allow saving the note

--- woocommerce-order-item-notes.php
+++ woocommerce-order-item-notes.php
@@ -44,6 +44,8 @@ if (!class_exists('WooCommerceOrderItemNotes')) {
 			add_action('init', array(&$this, 'redstar_woocommerceorderitemnotes_init'));
 			add_action('woocommerce_after_order_itemmeta', array(&$this, 'redstar_woocommerceorderitemnotes_admin_order_item_line_item_html'), 1000, 2);
             add_filter('woocommerce_hidden_order_itemmeta', array(&$this, 'redstar_woocommerceorderitemnotes_hidden_order_itemmeta'));
+            add_filter('wp_insert_post_data', array(&$this, 'redstar_woocommerceorderitemnotes_insert_post_data'), 10, 2);
+			add_action('pre_post_update', array(&$this, 'redstar_woocommerceorderitemnotes_pre_post_update'), 10, 2);
 		}
 
         function redstar_woocommerceorderitemnotes_init() {
@@ -62,6 +64,31 @@ if (!class_exists('WooCommerceOrderItemNotes')) {
         function redstar_woocommerceorderitemnotes_hidden_order_itemmeta($hidden_itemmeta) {
             array_push($hidden_itemmeta, '_order_item_note');
             return $hidden_itemmeta;
+        }
+
+        function redstar_woocommerceorderitemnotes_insert_post_data($data, $postarr) {
+            foreach ($postarr as $post_field => $post_value) {
+                if (!str_starts_with($post_field, 'order_item_note_')) {
+                    continue;
+                }
+
+                $data[$post_field] = $post_value;
+            }
+
+            return $data;
+        }
+
+        function redstar_woocommerceorderitemnotes_pre_post_update($post_id, $data) {            
+            foreach ($data as $post_field => $post_value) {
+                if (!str_starts_with($post_field, 'order_item_note_')) {
+                    continue;
+                }
+
+                $order_item_id = intval(substr($post_field, 16));
+                $order_item = new WC_Order_Item_Product($order_item_id);
+                $order_item->update_meta_data('_order_item_note', $post_value);
+                $order_item->save();
+            }
         }
     }
 }
