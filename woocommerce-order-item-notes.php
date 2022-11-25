d4073c4f8d2a6162f8ced0873050c0399e217484
Peter Morlion <peter.morlion@gmail.com>
2022-11-25 14:05:27 +0100

    Load notes if present in DB

--- woocommerce-order-item-notes.php
+++ woocommerce-order-item-notes.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Order Item Notes
 Plugin URI: https://redstar.be
 Description: A WooCommerce plugin to add notes or comments to individual order items of an order.
-Version: 1.0
+Version: 2.0
 Author: Peter Morlion
 Author URI: https://redstar.be
 Text Domain: woocommerce-order-item-notes
@@ -42,10 +42,24 @@ if (!class_exists('WooCommerceOrderItemNotes')) {
 	{
 		function __construct() {
 			add_action('init', array(&$this, 'redstar_woocommerceorderitemnotes_init'));
+			add_action('woocommerce_after_order_itemmeta', array(&$this, 'redstar_woocommerceorderitemnotes_admin_order_item_line_item_html'), 1000, 2);
 		}
-    }
 
-    function redstar_woocommerceorderitemnotes_init() {
-        load_plugin_textdomain('woocommerce-order-item-notes', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+        function redstar_woocommerceorderitemnotes_init() {
+            load_plugin_textdomain('woocommerce-order-item-notes', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+        }
+    
+        function redstar_woocommerceorderitemnotes_admin_order_item_line_item_html($item_id, $item) {
+            $notes = $item->get_meta('_order_item_note');
+            
+            echo '<h4>' . __('Notes', 'woocommerce-order-item-notes') . '</h4>';            
+            echo '<textarea name="order_item_note_' . $item_id . '" spellcheck="true" autocomplete="off">';
+            echo $notes;
+            echo '</textarea>';
+        }
     }
 }
+
+new WooCommerceOrderItemNotes();
+
+?>
