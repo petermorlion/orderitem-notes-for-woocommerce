cac03c001df0ca1cfbdaf132cd3d02beafff6de4
Peter Morlion <peter.morlion@gmail.com>
2023-08-16 14:28:51 +0200

    Format file

--- order-item-notes-for-woocommerce.php
+++ order-item-notes-for-woocommerce.php
@@ -22,57 +22,62 @@ defined('ABSPATH') || exit;
  */
 function redstar_woocommerceorderitemnotes_activate()
 {
-	// Your activation logic goes here.
+    // Your activation logic goes here.
 }
 register_activation_hook(__FILE__, 'redstar_woocommerceorderitemnotes_activate');
 
 function redstar_woocommerceorderitemnotes_deactivate()
 {
-	// Your deactivation logic goes here.
+    // Your deactivation logic goes here.
 
-	// Don't forget to:
-	// Remove Scheduled Actions
-	// Remove Notes in the Admin Inbox
-	// Remove Admin Tasks
+    // Don't forget to:
+    // Remove Scheduled Actions
+    // Remove Notes in the Admin Inbox
+    // Remove Admin Tasks
 }
 register_deactivation_hook(__FILE__, 'redstar_woocommerceorderitemnotes_deactivate');
 
 if (!class_exists('WooCommerceOrderItemNotes')) {
-	class WooCommerceOrderItemNotes
-	{
-		function __construct() {
-			add_action('init', array(&$this, 'redstar_woocommerceorderitemnotes_init'));
-			add_action('woocommerce_after_order_itemmeta', array(&$this, 'redstar_woocommerceorderitemnotes_after_order_itemmeta'), 1000, 2);
+    class WooCommerceOrderItemNotes
+    {
+        function __construct()
+        {
+            add_action('init', array(&$this, 'redstar_woocommerceorderitemnotes_init'));
+            add_action('woocommerce_after_order_itemmeta', array(&$this, 'redstar_woocommerceorderitemnotes_after_order_itemmeta'), 1000, 2);
             add_filter('woocommerce_hidden_order_itemmeta', array(&$this, 'redstar_woocommerceorderitemnotes_hidden_order_itemmeta'));
-			add_action('pre_post_update', array(&$this, 'redstar_woocommerceorderitemnotes_pre_post_update'), 10, 2);
-		}
-
-        function redstar_woocommerceorderitemnotes_init() {
-            load_plugin_textdomain('order-item-notes-for-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+            add_action('pre_post_update', array(&$this, 'redstar_woocommerceorderitemnotes_pre_post_update'), 10, 2);
         }
-    
-        function redstar_woocommerceorderitemnotes_after_order_itemmeta($item_id, $item) {
+
+        function redstar_woocommerceorderitemnotes_init()
+        {
+            load_plugin_textdomain('order-item-notes-for-woocommerce', false, dirname(plugin_basename(__FILE__)) . '/languages');
+        }
+
+        function redstar_woocommerceorderitemnotes_after_order_itemmeta($item_id, $item)
+        {
             $notes = $item->get_meta('_order_item_note');
-            ?>
+?>
             <h4>
-            <?php
-            esc_html_e('Notes', 'order-item-notes-for-woocommerce');
-            ?>
+                <?php
+                esc_html_e('Notes', 'order-item-notes-for-woocommerce');
+                ?>
             </h4>
             <textarea name="order_item_note_<?php echo esc_attr($item_id); ?>" spellcheck="true" autocomplete="off">
             <?php
             echo esc_textarea($notes);
             ?>
             </textarea>
-            <?php
+<?php
         }
 
-        function redstar_woocommerceorderitemnotes_hidden_order_itemmeta($hidden_itemmeta) {
+        function redstar_woocommerceorderitemnotes_hidden_order_itemmeta($hidden_itemmeta)
+        {
             array_push($hidden_itemmeta, '_order_item_note');
             return $hidden_itemmeta;
         }
 
-        function redstar_woocommerceorderitemnotes_pre_post_update($post_id, $data) {            
+        function redstar_woocommerceorderitemnotes_pre_post_update($post_id, $data)
+        {
             foreach ($data as $post_field => $post_value) {
                 if (!str_starts_with($post_field, 'order_item_note_')) {
                     continue;
